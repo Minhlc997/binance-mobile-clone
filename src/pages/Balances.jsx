0543a6297c2ba23aae24c7f44221ab53b545fee4
Minhlc997 <Mle20468@gmail.com>
2025-03-16 20:57:12 +0700

    Create Balances.jsx

---
+++ src/pages/Balances.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Balances.module.css";
+
+const Balances = () => {
+  return (
+    <div className="balances">
+      {/* Số dư tài khoản */}
+    </div>
+  );
+};
+
+export default Balances;
